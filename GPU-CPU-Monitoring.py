45e0db9c24c59a0f179b9bf519941a06f15f64f8
Arijit Ghosh <arijitghosh120403@gmail.com>
2023-04-02 13:10:33 +0530

    Writing the CPU usage in a CSV file

---
+++ GPU-CPU-Monitoring.py
@@ -0,0 +1,25 @@
+import csv
+import psutil
+import time
+
+interval = 2
+
+output_file = "CPU_MONITORED_DATA.csv"
+
+with open(output_file, "w", newline="") as f:
+    writer = csv.writer(f)
+    writer.writerow(["Timestamp", "CPU %", "Cores Used"])
+
+while True:
+    timestamp = time.time()
+
+    cpu_percent = psutil.cpu_percent()
+    cores_used = psutil.cpu_count(logical=False)
+
+    print(f"CPU: {cpu_percent:.1f}% ({cores_used} cores used)")
+
+    with open(output_file, "a", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow([timestamp, cpu_percent, cores_used])
+
+    time.sleep(interval)
